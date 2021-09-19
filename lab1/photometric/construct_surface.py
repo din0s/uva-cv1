d08fbb38ebcd95b897b90aa7a244757110673871
dinos <papakosk@csd.auth.gr>
2021-09-19 21:37:28 +0200

    Photometric stereo (#1)

--- lab1/photometric/construct_surface.py
+++ lab1/photometric/construct_surface.py
@@ -1,7 +1,56 @@
 import numpy as np
 
-def construct_surface(p, q, path_type='column'):
+def __constr_col_major(p, q):
+    """
+        ================
+        Your code here
+        ================
+        % top left corner of height_map is zero
+        % for each pixel in the left column of height_map
+        %   height_value = previous_height_value + corresponding_q_value
 
+        % for each row
+        %   for each element of the row except for leftmost
+        %       height_value = previous_height_value + corresponding_p_value
+
+    """
+    h, w = p.shape
+    height_map = np.zeros([h, w])
+
+    height_map[:, 0] = np.cumsum(q[:, 0])
+
+    for row in range(h):
+        for col in range(1, w):
+            height_map[row, col] = height_map[row, col - 1] + p[row, col]
+
+    return height_map
+
+def __constr_row_major(p, q):
+    """
+        ================
+        Your code here
+        ================
+        % top left corner of height_map is zero
+        % for each pixel in the top row of height_map
+        %   height_value = previous_height_value + corresponding_p_value
+
+        % for each column
+        %   for each element of the column except for topmost
+        %       height_value = previous_height_value + corresponding_q_value
+
+    """
+    h, w = p.shape
+    height_map = np.zeros([h, w])
+
+    height_map[0, :] = np.cumsum(p[0, :])
+
+    for col in range(w):
+        for row in range(1, h):
+            height_map[row, col] = height_map[row - 1, col] + q[row, col]
+
+    return height_map
+
+def construct_surface(p, q, path_type='column'):
     '''
     CONSTRUCT_SURFACE construct the surface function represented as height_map
        p : measures value of df / dx
@@ -10,36 +59,21 @@ def construct_surface(p, q, path_type='column'):
        'row', or 'average'
        height_map: the reconstructed surface
     '''
-    
+
     h, w = p.shape
     height_map = np.zeros([h, w])
-    
+
     if path_type=='column':
-        """
-        ================
-        Your code here
-        ================
-        % top left corner of height_map is zero
-        % for each pixel in the left column of height_map
-        %   height_value = previous_height_value + corresponding_q_value
-        
-        % for each row
-        %   for each element of the row except for leftmost
-        %       height_value = previous_height_value + corresponding_p_value
-        
-        """
+        height_map = __constr_col_major(p, q)
+
     elif path_type=='row':
-        """
-        ================
-        Your code here
-        ================
-        """
+        height_map = __constr_row_major(p, q)
+
+
     elif path_type=='average':
-        """
-        ================
-        Your code here
-        ================
-        """
-        
+        hm_c = __constr_col_major(p, q)
+        hm_r = __constr_row_major(p, q)
+        height_map = (hm_c + hm_r) / 2
+
     return height_map
-        
+
--- lab1/photometric/photometric_stereo.py
+++ lab1/photometric/photometric_stereo.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import os
+
+from numpy.linalg import norm
 from utils import *
 from estimate_alb_nrm import estimate_alb_nrm
 from check_integrability import check_integrability
@@ -8,24 +10,58 @@ from construct_surface import construct_surface
 
 print('Part 1: Photometric Stereo\n')
 
-def photometric_stereo(image_dir='./SphereGray5/' ):
-
+def photometric_stereo(image_dir='./images/SphereGray5/', rgb=False):
     # obtain many images in a fixed view under different illumination
     print('Loading images...\n')
-    [image_stack, scriptV] = load_syn_images(image_dir)
-    [h, w, n] = image_stack.shape
+    if rgb:
+        img_stack_rgb, scriptV_rgb = [], []
+        for channel in range(3):
+            [image_stack, scriptV] = load_syn_images(image_dir, channel)
+            img_stack_rgb.append(image_stack)
+            scriptV_rgb.append(scriptV)
+        [h, w, n] = img_stack_rgb[0].shape
+    else:
+        [image_stack, scriptV] = load_syn_images(image_dir)
+        [h, w, n] = image_stack.shape
+
     print('Finish loading %d images.\n' % n)
 
     # compute the surface gradient from the stack of imgs and light source mat
     print('Computing surface albedo and normal map...\n')
-    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV)
+    if rgb:
+        albedo = np.empty((h, w, 3))
+        normals_rgb = []
+        for channel in range(3):
+            [_albedo, _normals] = estimate_alb_nrm(img_stack_rgb[channel], scriptV_rgb[channel])
+            _albedo = cv2.normalize(_albedo, None, 0.0, 1.0, cv2.NORM_MINMAX)
+            albedo[:, :, 2 - channel] = _albedo
+            normals_rgb.append(_normals)
+        normals = np.mean(normals_rgb, axis=0)
 
+        normals_norm = np.linalg.norm(normals, axis=2)
+        for i in range(3):
+            normals[:, :, i] /= normals_norm
+        normals[normals != normals] = 0
+
+    else:
+        [albedo, normals] = estimate_alb_nrm(image_stack, scriptV)
 
     # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
     print('Integrability checking\n')
     [p, q, SE] = check_integrability(normals)
 
-    threshold = 0.005;
+    # thresholds = np.logspace(-3.5, -1, 20)
+    # outliers = [100 * np.sum(SE > t) / len(SE.flatten()) for t in thresholds]
+    # plt.figure()
+    # plt.grid()
+    # plt.plot(thresholds, outliers, 'o-')
+    # plt.plot([thresholds[7]], [outliers[7]], 'r.')
+    # plt.annotate("%.4f" % thresholds[7], (thresholds[7] + 0.5 * 1e-3, outliers[7]))
+    # plt.xlabel("threshold")
+    # plt.ylabel("outliers %")
+    # plt.xscale('log')
+    # plt.show()
+    threshold = 0.0026
     print('Number of outliers: %d\n' % np.sum(SE > threshold))
     SE[SE <= threshold] = float('nan') # for good visualization
 
@@ -36,12 +72,12 @@ def photometric_stereo(image_dir='./SphereGray5/' ):
     show_results(albedo, normals, height_map, SE)
 
 ## Face
-def photometric_stereo_face(image_dir='./yaleB02/'):
+def photometric_stereo_face(image_dir='./images/yaleB02/'):
     [image_stack, scriptV] = load_face_images(image_dir)
     [h, w, n] = image_stack.shape
     print('Finish loading %d images.\n' % n)
     print('Computing surface albedo and normal map...\n')
-    albedo, normals = estimate_alb_nrm(image_stack, scriptV)
+    albedo, normals = estimate_alb_nrm(image_stack, scriptV, False)
 
     # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
     print('Integrability checking')
@@ -56,7 +92,7 @@ def photometric_stereo_face(image_dir='./yaleB02/'):
 
     # show results
     show_results(albedo, normals, height_map, SE)
-    
+
 if __name__ == '__main__':
-    # photometric_stereo('./SphereGray5/')
+    # photometric_stereo('./images/MonkeyColor/', True)
     photometric_stereo_face()
