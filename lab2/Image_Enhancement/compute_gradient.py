33968974136de2073efd9d9ec60c407a6b3cd1a7
orpheous1 <orpheous18@yahoo.gr>
2021-09-26 21:30:33 +0200

    compute gradients cleanup

--- lab2/Image_Enhancement/compute_gradient.py
+++ lab2/Image_Enhancement/compute_gradient.py
@@ -8,35 +8,45 @@ def compute_gradient(image):
     Gx = convolve2d(image, kernel_x, mode='same', boundary='symm')
     Gy = convolve2d(image, kernel_y, mode='same', boundary='symm')
     im_magnitude = np.sqrt(Gx * Gx + Gy * Gy)
-    Gx[Gx == 0] = 1e-16
-    im_direction = np.arctan(Gy / Gx)
+    im_direction = np.arctan2(Gy, Gx)
     return Gx, Gy, im_magnitude, im_direction
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import cv2
+
     image = plt.imread('images/image2.jpg')
     G_x, G_y, mag, dire = compute_gradient(image)
-    fig, axs = plt.subplots(1, 5, figsize=(15, 15))
+    fig, axs = plt.subplots(2, 3, figsize=(20, 20))
 
-    axs[0].imshow(image, cmap='gray')
-    axs[0].set_title('Initial Image')
+    axs[0][0].imshow(image, cmap='gray')
+    axs[0][0].set_title('Initial Image', fontsize=20)
 
-    axs[1].imshow(G_x, cmap='gray')
-    axs[1].set_title('Gradient x')
+    axs[0][1].imshow(G_x, cmap='gray')
+    axs[0][1].set_title('Gradient x', fontsize=20)
 
-    axs[2].imshow(G_y, cmap='gray')
-    axs[2].set_title('Gradient y')
-
-    axs[3].imshow(mag, cmap='gray')
-    axs[3].set_title('Gradient Magnitude')
+    axs[0][2].imshow(G_y, cmap='gray')
+    axs[0][2].set_title('Gradient y', fontsize=20)
     
     norm_dire = cv2.normalize(dire, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    axs[4].imshow(norm_dire, cmap='gray')
-    axs[4].set_title('Gradient Direction')
-    
+    axs[1][0].imshow(mag, cmap='gray')
+    axs[1][0].set_title('Gradient Magnitude', fontsize=20)
+
+    axs[1][1].imshow(norm_dire, cmap='gray')
+    axs[1][1].set_title('Gradient Direction', fontsize=20)
+
+    mag_dire = cv2.normalize(mag, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    R = np.multiply(mag_dire, np.sin(norm_dire))
+    G = np.multiply(mag_dire, np.cos(norm_dire))
+    B = np.zeros_like(R)
+    color_dire = np.dstack((R, G, B))
+    color_dire = cv2.normalize(color_dire, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    axs[1][2].imshow(color_dire)
+    axs[1][2].set_title('Gradient Magnitude and Direction', fontsize=20)
+
+
     fig.tight_layout()
     plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
     plt.show()
