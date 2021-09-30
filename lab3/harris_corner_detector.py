7f4615b025d83b0d7a80beac6bc3719c1e6a0063
Dinos <papakosk@csd.auth.gr>
2021-09-30 03:40:11 +0200

    Implement Harris corner detector

---
+++ lab3/harris_corner_detector.py
@@ -0,0 +1,60 @@
+from scipy.ndimage import gaussian_filter, rotate
+from utils import image_derivatives, normal2chan
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def detect_corners(img: np.ndarray, window: int = 5, threshold: float = 0.7) -> tuple:
+    img = normal2chan(img)
+    Ix, Iy = image_derivatives(img)
+
+    A = gaussian_filter(Ix ** 2, sigma=1)
+    B = gaussian_filter(Ix * Iy, sigma=1)
+    C = gaussian_filter(Iy ** 2, sigma=1)
+    H = (A * C - B**2) - 0.04 * (A + C) ** 2
+
+    window = 5
+    h, w = img.shape
+    r, c = [], []
+    for x in range(window, h - window - 1):
+        for y in range(window, w - window - 1):
+            p = img[x, y]
+            region = img[x-window:x+window, y-window:y+window]
+            if p > threshold and region[region > p].size == 0:
+                r.append(x)
+                c.append(y)
+
+    return H, r, c
+
+def plot_corners(img: np.ndarray):
+    Ix, Iy = image_derivatives(img)
+    _, r, c = detect_corners(img)
+
+    _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15,5))
+    ax1.imshow(Ix) ; ax1.set_title(r"$\mathcal{I}_x$")
+    ax2.imshow(Iy) ; ax2.set_title(r"$\mathcal{I}_y$")
+    ax3.imshow(img, cmap='gray') ; ax3.scatter(c,r, s=1, c='red') ; ax3.set_title("Detected corners")
+    [ax.axis('off') for ax in (ax1,ax2,ax3)]
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    img_src = None
+
+    while img_src is None:
+        img_sel = input("Select image:\n1) Toy\n2) Doll\n[1/2]: ")
+        if img_sel == '1':
+            img_src = "./images/toy/0001.jpg"
+        elif img_sel == '2':
+            img_src = "./images/doll/0200.jpg"
+    
+    img = plt.imread(img_src)
+    
+    if input("Apply rotation? [y/N] ") == 'y':
+        img45 = rotate(img, 45)
+        img90 = rotate(img, 90)
+        plot_corners(img45)
+        plot_corners(img90)
+    else:
+        plot_corners(img)
---
+++ lab3/utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+import scipy.signal as sig
+
+def conv2d(image: np.ndarray, filter: np.ndarray) -> np.ndarray:
+    return sig.convolve2d(image, filter, mode='same', boundary='symm')
+
+def normal2chan(img: np.ndarray) -> np.ndarray:
+    dims = len(img.shape)
+    assert dims == 2 or dims == 3
+
+    if dims == 3:
+        # take the mean of all channels
+        img = np.mean(img, axis=2)
+    
+    if np.max(img) > 1:
+        # normalize to [0,1]
+        img = img / np.max(img)
+    
+    return img
+
+def image_derivatives(img: np.ndarray) -> tuple:
+    Gx = np.array([-1, 1])[:, np.newaxis]
+    Gy = Gx.T
+
+    # make sure the image is normalized and has 2 channels
+    img = normal2chan(img)
+
+    return conv2d(img, Gx), conv2d(img, Gy)
