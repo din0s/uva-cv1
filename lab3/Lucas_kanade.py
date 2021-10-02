cc71bb21736c4f65538cb2e82620652dd01355dc
orpheous1 <orpheous18@yahoo.gr>
2021-10-02 16:02:37 +0200

    image scale fix and rgb plot

--- lab3/Lucas_kanade.py
+++ lab3/Lucas_kanade.py
@@ -11,22 +11,22 @@ def blockify(image: np.ndarray, stride: int):
 
 
 def lucas_kanade(image1: np.ndarray, image2: np.ndarray) -> None:
+    rgb_image = image1
     STRIDE = 15
-    image1 = np.mean(image1, axis=2).astype(np.float32)
-    image2 = np.mean(image2, axis=2).astype(np.float32)
-    blocks1 = blockify(image1, STRIDE)
+    image1 = np.mean(image1, axis=2).astype(np.float32) / 255
+    image2 = np.mean(image2, axis=2).astype(np.float32) / 255
     G_x, G_y = image_derivatives(image1)
     blocks_G_x = blockify(G_x, STRIDE)
     blocks_G_y = blockify(G_y, STRIDE)
     As = [np.array([A, B]).T for A, B in zip(blocks_G_x, blocks_G_y)]
     Bs = blockify(image2 - image1, STRIDE)
-    vs = [np.linalg.inv(A.T @ A) @ A.T @ b for (A, b) in zip(As, Bs)]
+    vs = [np.linalg.pinv(A) @ b for (A, b) in zip(As, Bs)]
     plt.imshow(image1, cmap='gray')
     centers = [[(x, y) for y in range(7, image1.shape[0] - image1.shape[0] % 15, 15)] for x in range(7, image1.shape[1] - image1.shape[1] % 15, 15)]
     centers = sum(centers, [])
     x, y = np.array([l[0] for l in centers]), np.array([l[1] for l in centers])
-    plt.imshow(image1, cmap='gray')
-    plt.quiver(x, y, np.array(vs)[:, 0], np.array(vs)[:, 1], angles='xy', scale_units='xy', scale=50)
+    plt.imshow(rgb_image)
+    plt.quiver(x, y, np.array(vs)[:, 0], np.array(vs)[:, 1], angles='xy', scale_units='xy', scale=0.1)
     plt.show()
 
 if __name__ == '__main__':
