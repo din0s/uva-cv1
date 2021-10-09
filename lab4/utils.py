7b863f4bcf32b06073192bfb81b664289f8a66d7
Angelos <angelosnal97@gmail.com>
2021-10-09 20:32:04 +0200

    Changed RANSAC

--- lab4/utils.py
+++ lab4/utils.py
@@ -2,9 +2,11 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def imread_gray(file: str) -> np.ndarray:
     img = cv2.imread(file)
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
 def imshow(*imgs: np.ndarray, ax_titles: list = (), cmap: str = 'gray'):
     n, t = len(imgs), len(ax_titles)
@@ -32,15 +34,15 @@ def create_affine_matrix(m: np.ndarray, t: np.ndarray) -> np.ndarray:
     # translation matrix
     T = np.eye(3)
     T[0:2, 2] = t.squeeze()
-
     # result is given by mat mult
-    return R @ T
+    return T @ R
+
 
 def affine_wrap(img: np.ndarray, A: np.ndarray) -> np.ndarray:
     h, w = img.shape
     img_aff = np.zeros_like(img)
     for (y1, x1), val in np.ndenumerate(img):
-        xy1 = np.array([x1, y1,1]).T[:, np.newaxis]
+        xy1 = np.array([x1, y1, 1]).T[:, np.newaxis]
         xy2 = A @ xy1
 
         xy2 = xy2.flatten()
